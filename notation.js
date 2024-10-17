4b90af47b1d145e2221343f968b64aa716fdefc6
utshasaha16 <utsharoy12345@gmail.com>
2024-10-18 01:59:34 +0600

    and added notation

---
+++ notation.js
@@ -0,0 +1,12 @@
+const person = {
+    name: 'lal mia',
+    profession: 'traffic surgeon',
+    age: 40,
+    address: 'agrabad'
+}
+
+// dot notation
+const prof1 = person.profession;
+
+// bracket notation
+const prof2 = person['profession']
